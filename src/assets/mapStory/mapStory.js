5e2e7302624ddcd695a1c7fe6985e0112ccf16e5
Aaron Briggs <abriggs@contractor.usgs.gov>
2020-01-10 15:58:29 -0600

    tweaked story transitions

--- src/assets/mapStory/mapStory.js
+++ src/assets/mapStory/mapStory.js
@@ -4,27 +4,22 @@ export default {
             "id": "next-gen-intro",
             "class": "active",
             "flyToCommands": {
-                "bearing": 0,
-                "center": [-95.7129, 37.0902],
-                "zoom": 3,
-                "pitch": 0,
-                "speed": 0.2,
-                "curve": 1.5,
-                "essential": true
+
             },
             "title": "Next Generation Observing Systems",
             "content": "Emergency managers and water resource managers rely on the USGS stream gage network and the associated water delivery to provide data to address complex challenges involving water. This network is functioning but was designed many years ago and needs major upgrades to meet increasingly complex water challenges facing the Nation.",
             "extendedContent": false,
             "tourType": "none",
-            "layersToHide": [],
+            "layersToHide": ["monitoring-location-clusters", "monitoring-location-cluster-count",  "monitoring-location-unclustered-point"],
             "hiddenLayersToShow": []
         },
         {
             "id": "basin-intro",
             "class": "",
             "flyToCommands": {
-                "bearing": 30,
-                "center": [-75.600766, 39.762908],
+                "duration": 9000,
+                "bearing": 35,
+                "center": [-75.600766, 40.4467],
                 "zoom": 7,
                 "pitch": 0,
                 "speed": 0.2,
@@ -35,17 +30,18 @@ export default {
             "content": "The Delaware Basin area is a pilot to test out the Next Generation Water Observing System (NGWOS) that will support innovative modern water prediction and decision support systems in a nationally important, complex interstate river system. Lying in the densely populated corridor of the northeastern US, the 13,600 square mile Delaware River Basin stretches approximately 330 miles from headwaters in New York State to its confluence with the Atlantic Ocean. The basin includes approximately 12,800 square miles of land area, nearly 800 square miles of Bay and over 2,000 tributaries, including manythat are rivers in their own right. The northernmost tributaries to the Delaware River originate in the forested western slopes of the Catskill Mountains, which reach elevations of up to 4,000 feet. The East and West Branches meet at Hancock, NY where the Delaware River officially begins. The River descends about 800 feet on its journey to the Atlantic Ocean.",
             "extendedContent": false,
             "tourType": "none",
-            "layersToHide": [],
+            "layersToHide": ["monitoring-location-clusters", "monitoring-location-cluster-count",  "monitoring-location-unclustered-point"],
             "hiddenLayersToShow": []
         },
         {
             "id": "monitoring-location-intro",
             "class": "",
             "flyToCommands": {
-                "bearing": 350,
-                "center": [-74.9214, 42.4467],
-                "zoom": 8,
-                "pitch": 90,
+                "duration": 9000,
+                "bearing": 35,
+                "center": [-75.600766, 40.4467],
+                "zoom": 10,
+                "pitch": 0,
                 "speed": 0.2,
                 "curve": 1,
                 "essential": true,
@@ -80,8 +76,8 @@ export default {
             "id": "enhanced-monitoring-systems",
             "class": "",
             "flyToCommands": {
-                "duration": 15000,
-                "bearing": 100,
+                "duration": 9000,
+                "bearing": 0,
                 "center": [-74.9214, 40.4467],
                 "zoom": 7,
                 "pitch": 20,
@@ -99,8 +95,8 @@ export default {
             "id": "cameras",
             "class": "",
             "flyToCommands": {
-                "duration": 12000,
-                "bearing": 45,
+                "duration": 9000,
+                "bearing": 0,
                 "center": [-74.9214, 40.4467],
                 "zoom": 7,
                 "pitch": 20,
@@ -118,11 +114,11 @@ export default {
             "id": "temperature",
             "class": "",
             "flyToCommands": {
-                "duration": 8000,
-                "bearing": 180,
+                "duration": 9000,
+                "bearing": 0,
                 "center": [-74.9214, 40.4467],
                 "zoom": 7,
-                "pitch": 0,
+                "pitch": 20,
                 "speed": 0.5,
                 "essential": true
             },
@@ -137,11 +133,11 @@ export default {
             "id": "specific_conductance",
             "class": "",
             "flyToCommands": {
-                "duration": 8000,
-                "bearing": 180,
+                "duration": 9000,
+                "bearing": 0,
                 "center": [-74.9214, 40.4467],
                 "zoom": 7,
-                "pitch": 0,
+                "pitch": 20,
                 "speed": 0.5,
                 "essential": true
             },
@@ -156,7 +152,7 @@ export default {
             "id": "summary",
             "class": "",
             "flyToCommands": {
-                "duration": 15500,
+                "duration": 9500,
                 "bearing": 0,
                 "center": [-74.3867, 40.6134],
                 "zoom": 5,
@@ -169,7 +165,7 @@ export default {
                     "management goal. However, sea level rise may require new management measures, operations plans and/or additional water to maintain control of salinity.",
             "extendedContent": false,
             "tourType": "none",
-            "layersToHide": [],
+            "layersToHide": ["monitoring-location-clusters", "monitoring-location-cluster-count"],
             "hiddenLayersToShow": []
         }
     ]

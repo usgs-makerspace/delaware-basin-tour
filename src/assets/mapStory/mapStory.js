b31c6f145856c1a299d6f19e3b40845869012e47
Aaron Briggs <abriggs@contractor.usgs.gov>
2020-02-10 16:02:09 -0600

    fixed misspelling

--- src/assets/mapStory/mapStory.js
+++ src/assets/mapStory/mapStory.js
@@ -10,7 +10,7 @@ export default {
             "content": "Emergency managers and water resource managers rely on the USGS stream gage network and the associated water delivery to provide data to address complex challenges involving water. This network is functioning but was designed many years ago and needs major upgrades to meet increasingly complex water challenges facing the Nation.",
             "tourType": "none",
             "layersToHide": ["monitoring-location-clusters", "monitoring-location-cluster-count",  "monitoring-location-unclustered-point", "physiographic_provinces"],
-            "hiddenLayersToShow": ["new_gage_delaware_basin", "enhanced_gag_delaware_basin", "camera_delaware_basin", "specific_conductance_delaware_basin", "temperature_delaware_basin"],
+            "hiddenLayersToShow": ["new_gage_delaware_basin", "enhanced_gage_delaware_basin", "camera_delaware_basin", "specific_conductance_delaware_basin", "temperature_delaware_basin"],
             "D3Rings": false
         },
         {
@@ -30,7 +30,7 @@ export default {
             "content": "The Delaware Basin area is a pilot to test out the Next Generation Water Observing System (NGWOS) that will support innovative modern water prediction and decision support systems in a nationally important, complex interstate river system. Lying in the densely populated corridor of the northeastern US, the 13,600 square mile Delaware River Basin stretches approximately 330 miles from headwaters in New York State to its confluence with the Atlantic Ocean. The basin includes approximately 12,800 square miles of land area, nearly 800 square miles of Bay and over 2,000 tributaries, including manythat are rivers in their own right. The northernmost tributaries to the Delaware River originate in the forested western slopes of the Catskill Mountains, which reach elevations of up to 4,000 feet. The East and West Branches meet at Hancock, NY where the Delaware River officially begins. The River descends about 800 feet on its journey to the Atlantic Ocean.",
             "tourType": "none",
             "layersToHide": ["all_locations", "monitoring-location-clusters", "monitoring-location-cluster-count",  "monitoring-location-unclustered-point", "physiographic_provinces"],
-            "hiddenLayersToShow": ["new_gage_delaware_basin", "enhanced_gag_delaware_basin", "camera_delaware_basin", "specific_conductance_delaware_basin", "temperature_delaware_basin"],
+            "hiddenLayersToShow": ["new_gage_delaware_basin", "enhanced_gage_delaware_basin", "camera_delaware_basin", "specific_conductance_delaware_basin", "temperature_delaware_basin"],
             "D3Rings": false
         },
         {
@@ -50,7 +50,7 @@ export default {
             "content": "The USGS collects water related data from more than 10,000 gages in the basin.",
             "tourType": "none",
             "layersToHide": ["all_locations","physiographic_provinces"],
-            "hiddenLayersToShow": ["new_gage_delaware_basin", "enhanced_gag_delaware_basin", "camera_delaware_basin", "specific_conductance_delaware_basin", "temperature_delaware_basin"],
+            "hiddenLayersToShow": ["new_gage_delaware_basin", "enhanced_gage_delaware_basin", "camera_delaware_basin", "specific_conductance_delaware_basin", "temperature_delaware_basin"],
             "D3Rings": false
         },
         {
@@ -69,7 +69,7 @@ export default {
             "content": "In 2018, seventeen new monitoring systems were added to the Delaware River Basin region to address monitoring gaps to improve the quantification of the amount of water in the headwater and tributary streams and to track the salt front in the mainstem",
             "tourType": "none",
             "layersToHide": ["all_locations", "monitoring-location-clusters", "monitoring-location-cluster-count",  "monitoring-location-unclustered-point", "cameras", "specific_conductance", "enhanced_gage", "temperature", "physiographic_provinces"],
-            "hiddenLayersToShow": ["new_gage_delaware_basin", "enhanced_gag_delaware_basin", "camera_delaware_basin", "specific_conductance_delaware_basin", "temperature_delaware_basin"],
+            "hiddenLayersToShow": ["new_gage_delaware_basin", "enhanced_gage_delaware_basin", "camera_delaware_basin", "specific_conductance_delaware_basin", "temperature_delaware_basin"],
             "D3Rings": false
         },
         {
@@ -165,9 +165,9 @@ export default {
             "tourType": "none",
             "layersToHide": ["monitoring-location-clusters", "monitoring-location-cluster-count",  "monitoring-location-unclustered-point", "cameras", "enhanced_gage", "temperature", "new_gage", "specific_conductance","drb_boundary"],
             "hiddenLayersToShow": ["physiographic_provinces",
-                "new_gage_physiographic_region_1", "enhanced_gag_physiographic_region_1", "camera_physiographic_region_1", "specific_conductance_physiographic_region_1", "temperature_physiographic_region_1",
-                "new_gage_physiographic_region_2", "enhanced_gag_physiographic_region_2", "camera_physiographic_region_2", "specific_conductance_physiographic_region_2", "temperature_physiographic_region_2",
-                "new_gage_physiographic_region_3", "enhanced_gag_physiographic_region_3", "camera_physiographic_region_3", "specific_conductance_physiographic_region_3", "temperature_physiographic_region_3"
+                "new_gage_physiographic_region_1", "enhanced_gage_physiographic_region_1", "camera_physiographic_region_1", "specific_conductance_physiographic_region_1", "temperature_physiographic_region_1",
+                "new_gage_physiographic_region_2", "enhanced_gage_physiographic_region_2", "camera_physiographic_region_2", "specific_conductance_physiographic_region_2", "temperature_physiographic_region_2",
+                "new_gage_physiographic_region_3", "enhanced_gage_physiographic_region_3", "camera_physiographic_region_3", "specific_conductance_physiographic_region_3", "temperature_physiographic_region_3"
             ],
             "D3Rings": false
         },
@@ -188,7 +188,7 @@ export default {
                     "management goal. However, sea level rise may require new management measures, operations plans and/or additional water to maintain control of salinity.",
             "tourType": "all_locations",
             "layersToHide": ["monitoring-location-clusters", "monitoring-location-cluster-count", "cameras", "enhanced_gage", "temperature", "new_gage", "specific_conductance","drb_boundary","physiographic_provinces"],
-            "hiddenLayersToShow": ["drb_boundary", "new_gage_delaware_basin", "enhanced_gag_delaware_basin", "camera_delaware_basin", "specific_conductance_delaware_basin", "temperature_delaware_basin"],
+            "hiddenLayersToShow": ["drb_boundary", "new_gage_delaware_basin", "enhanced_gage_delaware_basin", "camera_delaware_basin", "specific_conductance_delaware_basin", "temperature_delaware_basin"],
             "D3Rings": true
         },
         {
@@ -207,7 +207,7 @@ export default {
             "content": "This is a place holder to serve as an example of functionality",
             "tourType": "none",
             "layersToHide": ["monitoring-location-clusters", "monitoring-location-cluster-count",  "monitoring-location-unclustered-point", "cameras", "enhanced_gage", "temperature", "new_gage", "specific_conductance","drb_boundary"],
-            "hiddenLayersToShow": ["new_gage_test_bed", "enhanced_gag_test_bed", "camera_test_bed", "specific_conductance_test_bed", "temperature_test_bed"],
+            "hiddenLayersToShow": ["new_gage_test_bed", "enhanced_gage_test_bed", "camera_test_bed", "specific_conductance_test_bed", "temperature_test_bed"],
             "D3Rings": false
         },
 	    {
